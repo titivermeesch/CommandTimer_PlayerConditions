7783d482a71319548653e04144f69bccc8ca3f06
Tristan Vermeesch <titivermeesch@gmail.com>
2025-10-11 11:01:05 +0200

    fix: change build structure to prevent java version errors

--- src/main/java/me/playbosswar/cmtplayerconditions/CommandTimerPlayerConditions.java
+++ src/main/java/me/playbosswar/cmtplayerconditions/CommandTimerPlayerConditions.java
@@ -45,7 +45,7 @@ public class CommandTimerPlayerConditions extends ConditionExtension {
 
     @Override
     public @NotNull String getVersion() {
-        return "1.4.2";
+        return "1.4.3";
     }
 
     public @NotNull ConditionRules getRules() {
