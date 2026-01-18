107bed2809f1e0129b0a7d0dab3af9719da3b457
Tristan Vermeesch <titivermeesch@gmail.com>
2026-01-18 15:01:43 +0100

    fix: compatibility issue with CMT v8.16

--- src/main/java/me/playbosswar/cmtplayerconditions/CommandTimerPlayerConditions.java
+++ src/main/java/me/playbosswar/cmtplayerconditions/CommandTimerPlayerConditions.java
@@ -45,7 +45,7 @@ public class CommandTimerPlayerConditions extends ConditionExtension {
 
     @Override
     public @NotNull String getVersion() {
-        return "1.5.1";
+        return "1.5.2";
     }
 
     public @NotNull ConditionRules getRules() {
